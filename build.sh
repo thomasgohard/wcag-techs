79a18287f02fde27ce0bb3819ac0b464229eb1e0
Jeremy S <jeresiv@gmail.com>
2013-08-14 16:45:55 -0400

    Update build process to include jekyll

--- build.sh
+++ build.sh
@@ -17,5 +17,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	git remote add $GH_REMOTE https://${GH_TOKEN}@github.com/$GH_ACCOUNT/$GH_REPOSITORY.git
 
 	git checkout -B $GH_PAGESBRANCH
+	
+	echo "Push to gh-pages branch"
 	git push -fq $GH_REMOTE $GH_PAGESBRANCH 2> /dev/null || error_exit "Unable to push to gh-pages."
 fi
