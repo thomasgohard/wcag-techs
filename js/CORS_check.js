483e4c850960a6e1e741ad930508adb54b430021
thomasgohard <thomas@gohard-soutar.com>
2013-08-19 11:14:24 -0400

    Added the substitution of the /edit/ portion of the Prose.io URL with /blob/ as GitHub returns a 404 on /edit/ links when not logged in.

--- js/CORS_check.js
+++ js/CORS_check.js
@@ -8,12 +8,13 @@
 	// Test that browser supports CORS (uses test from prose/boot.js). Intended to reroute to fallback when Prose won't work. 
 	if (!('withCredentials' in new XMLHttpRequest())) {
 		var edit_button = document.getElementById('edit-button');
-		//var request = false;
 
 		if (edit_button !== null) {
 			var edit_link = edit_button.getAttribute('href');
 
-			edit_button.setAttribute('href', edit_link.replace(/^http:\/\/prose\.io\/#/, 'https://github.com/'));
+			edit_link = edit_link.replace(/^http:\/\/prose\.io\/#/, 'https://github.com/').replace(/\/edit\//, '/blob/')
+
+			edit_button.setAttribute('href', edit_link);
 		}
 	}
 }());
