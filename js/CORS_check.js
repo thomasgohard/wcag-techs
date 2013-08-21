29aa8e5c6a3769cbe63dd4f532e2aba4c5bca991
thomasgohard <thomas@gohard-soutar.com>
2013-08-21 13:14:35 -0400

    Better integrated the edit link into the page layout.

--- js/CORS_check.js
+++ js/CORS_check.js
@@ -7,14 +7,14 @@
 
 	// Test that browser supports CORS (uses test from prose/boot.js). Intended to reroute to fallback when Prose won't work. 
 	if (!('withCredentials' in new XMLHttpRequest())) {
-		var edit_button = document.getElementById('edit-button');
+		var edit_link = document.getElementById('edit-link');
 
-		if (edit_button !== null) {
-			var edit_link = edit_button.getAttribute('href');
+		if (edit_link !== null) {
+			var edit_href = edit_link.getAttribute('href');
 
-			edit_link = edit_link.replace(/^http:\/\/prose\.io\/#/, 'https://github.com/').replace(/\/edit\//, '/blob/')
+			edit_href = edit_href.replace(/^http:\/\/prose\.io\/#/, 'https://github.com/').replace(/\/edit\//, '/blob/')
 
-			edit_button.setAttribute('href', edit_link);
+			edit_link.setAttribute('href', edit_href);
 		}
 	}
 }());
